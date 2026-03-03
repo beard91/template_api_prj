d291589483635c941191f6e17a853917808ac075
beard91 <enricosegatto.sw@gmail.com>
2026-03-03 18:35:09 +0100

    added possibility to log into log file

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,7 +1,15 @@
 import logging
 
-def setup_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s [%(name)s] %(message)s"
-    )
+def setup_logging(save_to_file=False, filename="app.log"):
+    if save_to_file: #save only into log file
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s | %(levelname)s | [%(name)s] %(message)s",
+            filename=filename,
+            filemode='a'  #Append to the log file
+        )
+    else: #print to console
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s | %(levelname)s | [%(name)s] %(message)s"
+        )
